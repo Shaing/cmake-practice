b2f81929d333044bbf7d3c413e41279dd7f8bbf4
ah <allen_huang@innodisk.com>
2022-01-10 13:10:07 +0800

    chore: add share library

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 // #include <iostream>
 #include "inc/hw.h"
+#include <iostream>
+#include <chrono>
+
+using namespace std;
 
 int main(int argc, char** argv)
 {
+	chrono::steady_clock::time_point t1 = chrono::steady_clock::now();
 	helloWorld();
+	chrono::steady_clock::time_point t2 = chrono::steady_clock::now();
+	cout << chrono::duration_cast<chrono::seconds>(t2 - t1).count() << " s. \n";
 	return 0;
 }
--- src/hw.cpp
+++ src/hw.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <unistd.h>
+#include "hw.h"
 
 int helloWorld()
 {
 	std::cout << "Hello World" << std::endl;
+	sleep(1);
 	return 0;
 }
