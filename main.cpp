0498cdb41ac34afb185468c817325b64aa9c2652
ah <allen_huang@innodisk.com>
2023-10-05 14:38:29 +0800

    feat: add VideoWriter

--- main.cpp
+++ main.cpp
@@ -17,10 +17,19 @@ int run(string t_name, string input_src, string window_name)
     }
 
     Mat frame;
+    Mat rframe;
     Mat gray;
+    VideoWriter vw;
+    vw.open(string{t_name + ".mp4"}.c_str(), VideoWriter::fourcc('m', 'p', '4', 'v'), 30.0, Size(540, 540));
+    if (!vw.isOpened())
+    {   
+        cout << "Can't open VideoWriter. Exiting...\n";
+        return -1;
+    }
+
     int c = 0;
-    auto t1 = chrono::high_resolution_clock::now();
     while (true) {
+        auto t1 = chrono::high_resolution_clock::now();
         bool ret = cap.read(frame); // or cap >> frame;
         if (!ret) {
             cout << "Can't receive frame (stream end?). Exiting ...\n";
@@ -28,18 +37,24 @@ int run(string t_name, string input_src, string window_name)
         }
         ++c;
         // cvtColor(frame, gray, COLOR_BGR2GRAY);
+        resize(frame, rframe, Size(540, 540), INTER_LINEAR);
 
-        imshow(window_name, frame);
+
+        // imshow(window_name, frame);
+        imshow(window_name, rframe);
         //imshow("live", gray);
         // sleep(1); // for testing FPS 1 sec.
+
+        vw.write(rframe);
+
+        auto t2 = chrono::high_resolution_clock::now();
+        chrono::duration<double, milli> fp_ms = t2 - t1;
+        cout << "[ " << t_name << "]" << "FPS: " << 1 / (fp_ms.count() / 1000) << endl;
 
         if (waitKey(1) == 'q') {
             break;
         }
     }
-    auto t2 = chrono::high_resolution_clock::now();
-    chrono::duration<double, milli> fp_ms = t2 - t1;
-    cout << "[ " << t_name << "]" << "FPS: " << c / (fp_ms.count() / 1000) << endl;
     cout << "[ " << t_name << "]" << "Totoal frame: " << c << endl;
     return 0;
 }
